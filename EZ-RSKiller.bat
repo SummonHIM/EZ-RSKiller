836fb189e42e87669686ed36f4ba461956764b47
SummonHIM <2239760702@qq.com>
2022-02-21 19:55:06 +0800

    6.0

--- EZ-RSKiller.bat
+++ EZ-RSKiller.bat
@@ -1,117 +1,165 @@
 :Head
 @echo off
-set version=5.0
+set version=6.0
 mode con cols=66 lines=30
+
 ver|find "Windows XP" > NUL && set System=WinXP
 if "%System%"=="WinXP" (
-  set DocumentsLocale=C:\Documents and Settings\%username%\My Documents\
-  set DesktopLocale=C:\Documents and Settings\%username%\桌面\
-  copy %0 "C:\Documents and Settings\%username%\My Documents\EZ.bat"
-  goto NoAdmin
+    set DocumentsLocale=C:\Documents and Settings\%username%\My Documents\
+    goto NoAdmin
 ) else (
-  set DocumentsLocale=C:\Users\%username%\Documents\
-  set DesktopLocale=C:\Users\%username%\Desktop\
-  copy %0 "C:\Users\%username%\Documents\EZ.bat"
+    set DocumentsLocale=C:\Users\%username%\Documents\
 )
-echo /////     请允许管理员权限！     /////
+
+echo,
+echo     /////     该脚本需要使用管理员权限才能正常运行。     /////
+echo     /////               请允许管理员权限！               /////
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit >nul
 :NoAdmin
+
 title 红蜘蛛杀手 V%version%
 cls
-echo                            红蜘蛛杀手
-echo                               V%version%
+echo                          红蜘蛛杀手 V%version%
+echo                        ez.yxsw1802.com.cn
 echo                     Copyright %date:~0,4% SummonHIM.
-if exist "%DocumentsLocale%REDLocal.ini" (goto CheckRedAgentStatus) else (goto FirstTimeRun)
+if exist "%~dp0REDLocal.cfg" (
+    goto CheckRedAgentStatus
+) else (
+    goto RegSearch
+)
 
-:FirstTimeRun
+:RegSearch
 color 4E
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo 首次启动，正在查找红蜘蛛具体位置。请稍候...
+echo,
+echo REDLocal.cfg 文件尽量不要删除，该文件用于定位红蜘蛛具体位置！
+
+for /f "tokens=1,2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\3000soft\Red Spider" /v "AgentCommand" ^| find /i "AgentCommand"') do set RedAgentPath=%%k
+if "%regValue%"=="" for /f "tokens=1,2,*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\3000soft\Red Spider" /v "AgentCommand" ^| find /i "AgentCommand"') do set RedAgentPath=%%k
+set RedAgentPath=%RedAgentPath:~0,-12%
+if not exist "%RedAgentPath%REDAgent.exe" (
+    if not exist "%RedAgentPath%REDAgent.disabled" (
+        goto RedAgentNotFound
+    ) else (
+        goto FTRContinue
+    )
+) else (
+    goto FTRContinue
+)
+pause
+
+:FullDiskSearch
+color 4E
+echo,
+echo 请输入欲扫描的盘符：
+echo 请不要将“:\”填入。
+echo 例如我想扫描 C 盘则直接输入：C
+echo,
+set /p FTRDisk=请输入：
+echo,
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo 正在查找位于 “%FTRDisk%:\” 盘的红蜘蛛具体位置。请稍候...
 echo 查找速度可能较慢，请耐心等待！
 echo,
-echo 位于 %DocumentsLocale%REDLocal.ini 的文件尽量不要删除，该文件用于定位红蜘蛛具体位置！
+echo REDLocal.cfg 文件尽量不要删除，该文件用于定位红蜘蛛具体位置！
 echo,
 set FindData=0
+
 :FTRReFind
-for /r c:\ %%i in (NetTalk.exe*)do (
-  set RedAgentPath=%%~dpi
-  if "%FindData%"== "1" (
-    set RedAgentPath1=%%~dpi
-  )
-  if "%FindData%"== "2" (
-    set RedAgentPath2=%%~dpi
-  )
-  if "%FindData%" == "3" (
-    if "%RedAgentPath1%" == "%RedAgentPath2%" (goto FTRFind7)
-    set FindData=0
-  )
-  set /a FindData=%FindData%+1
-  echo 查询到路径（红蜘蛛 6.0）：%%~dpi
-  if not exist %%~dpiREDAgent.exe (
-    if not exist %%~dpiREDAgent0.exe (goto FTRReFind) else (goto FTRContinue)
-  ) else (
-    goto FTRContinue
-  )
+for /r %FTRDisk%:\ %%i in (NetTalk.exe*)do (
+    set RedAgentPath=%%~dpi
+    if "%FindData%"== "1" set RedAgentPath1=%%~dpi
+    if "%FindData%"== "2" set RedAgentPath2=%%~dpi
+    if "%FindData%" == "3" (
+        if "%RedAgentPath1%" == "%RedAgentPath2%" (
+            goto FTRFind
+        )
+        set FindData=0
+    )
+    set /a FindData=%FindData%+1
+    echo 查询到路径（红蜘蛛 6.0）：%%~dpi
+    if not exist %%~dpiREDAgent.exe (
+        if not exist %%~dpiREDAgent.disabled ( 
+            goto FTRReFind
+        ) else ( 
+            goto FTRContinue
+        )
+    ) else ( 
+        goto FTRContinue 
+    )
 )
-:FTRFind7
-for /r c:\ %%i in (edpaper.exe*)do (
-  set RedAgentPath=%%~dpi
-  if "%FindData%"== "1" (
-    set RedAgentPath1=%%~dpi
-  )
-  if "%FindData%"== "2" (
-    set RedAgentPath2=%%~dpi
-  )
-  if "%FindData%" == "3" (
-    if "%RedAgentPath1%" == "%RedAgentPath2%" (goto RedAgentNotFound)
-    set FindData=0
-  )
-  set /a FindData=%FindData%+1
-  echo 查询到路径（红蜘蛛 7.0）：%%~dpi
-  if not exist %%~dpiREDAgent.exe (
-    if not exist %%~dpiREDAgent0.exe (goto FTRReFind) else (goto FTRContinue)
-  ) else (
-    goto FTRContinue
-  )
+
+:FTRFind
+for /r %FTRDisk%:\ %%i in (edpaper.exe*)do (
+    set RedAgentPath=%%~dpi
+    if "%FindData%"== "1" set RedAgentPath1=%%~dpi
+    if "%FindData%"== "2" set RedAgentPath2=%%~dpi
+    if "%FindData%" == "3" (
+        if "%RedAgentPath1%" == "%RedAgentPath2%" goto RedAgentNotFound
+        set FindData=0
+    )
+    set /a FindData=%FindData%+1
+    echo 查询到路径（红蜘蛛 7.0）：%%~dpi
+    if not exist %%~dpiREDAgent.exe (
+        if not exist %%~dpiREDAgent.disabled (
+            goto FTRReFind
+        ) else (
+            goto FTRContinue
+        )
+    ) else (
+        goto FTRContinue
+    )
 )
+
 :FTRContinue
 echo,
 echo 查找结束，红蜘蛛软件的路径为：
 echo %RedAgentPath%
 echo,
-echo 正在将路径保存到 %DocumentsLocale%REDLocal.ini
-echo %RedAgentPath%>"%DocumentsLocale%REDLocal.ini"
-if not exist "%DocumentsLocale%REDLocal.ini" (goto NoAccess) else (echo 保存成功！)
+echo 正在将路径保存到 REDLocal.cfg
+echo %RedAgentPath%>"%~dp0REDLocal.cfg"
+if not exist "%~dp0REDLocal.cfg" (
+    goto NoAccess
+) else (
+    echo 保存成功！
+)
 
 :CheckRedAgentStatus
-set /P RedAgentPath=<"%DocumentsLocale%REDLocal.ini"
+set /P RedAgentPath=<"%~dp0REDLocal.cfg"
 if exist "%RedAgentPath%REDAgent.exe" goto ShutdownRedAgent
-if exist "%RedAgentPath%REDAgent0.exe" (goto StartRedAgent) ELSE (goto RedAgentNotFound)
+if exist "%RedAgentPath%REDAgent.disabled" (
+    goto StartRedAgent
+) else (
+    goto RedAgentNotFound
+)
 
 :RedAgentNotFound
 cls
 color 4E
-echo                            红蜘蛛杀手
-echo                               V%version%
+echo                          红蜘蛛杀手 V%version%
+echo                        ez.yxsw1802.com.cn
 echo                     Copyright %date:~0,4% SummonHIM.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo 红蜘蛛软件路径：%RedAgentPath%
+echo 该路径未找到红蜘蛛软件：%RedAgentPath%
 echo,
-echo 本程序未查找到红蜘蛛软件！
+echo 本脚本未查找到红蜘蛛软件！
 echo 是否重新搜索红蜘蛛所在位置？
-echo （可以手动修改 %DocumentsLocale%REDLocal.ini 路径文件。）
+echo （可以手动修改 REDLocal.cfg 路径文件。）
 echo,
-echo Y＝是，N＝退出，E=自定义红蜘蛛路径。输入完毕则按回车键继续。
+echo [1] 尝试使用注册表方式查找（较快）
+echo [2] 尝试使用全盘扫描方式查找（较慢）
+echo [3] 尝试直接编辑 REDLocal.cfg 路径文件
+echo [4] 退出脚本
+echo 输入完毕则按回车键继续。
 set NotFoundYorN=
-set /p NotFoundYorN=[Y/N/E]:
-if "%NotFoundYorN%" == "y" set NotFoundYorN=Y
-if "%NotFoundYorN%" == "n" set NotFoundYorN=N
-if "%NotFoundYorN%" == "e" set NotFoundYorN=E
-if "%NotFoundYorN%" == "Y" goto FirstTimeRun
-if "%NotFoundYorN%" == "N" goto Exiting
-if "%NotFoundYorN%" == "E" goto EditRedLocal
+set /p NotFoundYorN=[1/2/3/4]:
+if "%NotFoundYorN%" == "1" goto RegSearch
+if "%NotFoundYorN%" == "2" goto FullDiskSearch
+if "%NotFoundYorN%" == "3" goto EditRedLocal
+if "%NotFoundYorN%" == "4" goto Exiting
+echo,
 echo 该命令 "%NotFoundYorN%" 不存在!
-echo 命令提示:Y＝是，N＝退出，E=自定义红蜘蛛路径(不区分大小写)
 pause
 cls
 goto RedAgentNotFound
@@ -122,11 +170,15 @@ echo 请将红蜘蛛软件的路径输入到此处。
 echo 例：C:\Program Files\3000soft\Red Spider\
 echo 请务必在路径最后面加一个下划线“\”，避免错误！
 echo,
-set /p RedAgentPath=
+set /p RedAgentPath=请输入：
 echo,
-echo 正在将路径保存到 %DocumentsLocale%REDLocal.ini
-echo %RedAgentPath%>"%DocumentsLocale%REDLocal.ini"
-if not exist "%DocumentsLocale%REDLocal.ini" (goto NoAccess) else (echo 保存成功！)
+echo 正在将路径保存到 REDLocal.cfg
+echo %RedAgentPath%>"%~dp0REDLocal.cfg"
+if not exist "%~dp0REDLocal.cfg" (
+    goto NoAccess
+) else (
+    echo 保存成功！
+)
 pause
 goto Head
 
@@ -135,9 +187,9 @@ color 4E
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo 正在执行一键关闭程序...
 taskkill /f /im REDAgent.exe /t
-rename "%RedAgentPath%REDAgent.exe" REDAgent0.exe
-if errorlevel 1 (goto NoAccess)
-echo 已将 REDAgent.exe 重命名为 REDAgent0.exe
+rename "%RedAgentPath%REDAgent.exe" REDAgent.disabled
+if errorlevel 1 goto NoAccess
+echo 已将 REDAgent.exe 重命名为 REDAgent.disabled
 echo 正在完成！
 set RedAgentStatus=Closed
 goto Finish
@@ -146,13 +198,13 @@ goto Finish
 color 4E
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo 确定要恢复红蜘蛛软件吗？
-echo 否则关闭此程序即可。
+echo 否则关闭此脚本即可。
 echo,
 echo 按任意键继续操作...
 pause >nul
-rename "%RedAgentPath%REDAgent0.exe" REDAgent.exe
-if errorlevel 1 (goto NoAccess)
-echo 已将 REDAgent0.exe 重命名为 REDAgent.exe
+rename "%RedAgentPath%REDAgent.disabled" REDAgent.exe
+if errorlevel 1 goto NoAccess
+echo 已将 REDAgent.disabled 重命名为 REDAgent.exe
 echo 正在强制启动红蜘蛛软件...
 start /d "%RedAgentPath%" REDAgent.exe
 echo 正在完成！
@@ -162,57 +214,24 @@ goto Finish
 :Finish
 color 2F
 cls
-echo                            红蜘蛛杀手
-echo                               V%version%
+echo                          红蜘蛛杀手 V%version%
+echo                        ez.yxsw1802.com.cn
 echo                     Copyright %date:~0,4% SummonHIM.
 echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if "%RedAgentStatus%" == "Closed" (
-echo                         红蜘蛛软件已关闭！
-echo 下次启动内容为：恢复红蜘蛛。
+    echo                         红蜘蛛软件已关闭！
+    echo 下次启动内容为：恢复红蜘蛛。
 ) ELSE (
-if "%RedAgentStatus%" == "Started" (
-echo                         红蜘蛛软件已恢复！
-echo 下次启动内容为：关闭红蜘蛛。
-)
-)
-echo,
-echo 本脚本易被管理员爆破，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
-echo 本脚本易被管理员爆破，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
-echo 本脚本易被管理员爆破，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
-msg * 本脚本易被管理员爆破，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
-if exist "%DesktopLocale%RSOKR.vbs" (
-  goto Exiting
+    if "%RedAgentStatus%" == "Started" (
+        echo                         红蜘蛛软件已恢复！
+        echo 下次启动内容为：关闭红蜘蛛。
+    )
 )
 echo,
-echo 是否设置一键运行本脚本（Ctrl+Alt+F4）？
-echo Y＝是，N＝退出。输入完毕则按回车键继续。
-set OneKeyRunYorN=
-set /p OneKeyRunYorN=[Y/N/E]:
-if "%OneKeyRunYorN%" == "y" set OneKeyRunYorN=Y
-if "%OneKeyRunYorN%" == "n" set OneKeyRunYorN=N
-if "%OneKeyRunYorN%" == "Y" goto OneKeyRun
-if "%OneKeyRunYorN%" == "N" goto Exiting
-echo 该命令 "%OneKeyRunYorN%" 不存在!
-echo 命令提示:Y＝是，N＝退出(不区分大小写)
-pause
-cls
-goto Finish
-
-:OneKeyRun
-echo Set WshShell = Wscript.CreateObject("Wscript.Shell") > "%DesktopLocale%RSOKR.vbs"
-echo, >> "%DesktopLocale%RSOKR.vbs"
-echo Set oMyShortcut = WshShell.CreateShortcut("a_key.lnk") >> "%DesktopLocale%RSOKR.vbs"
-echo OMyShortcut.TargetPath = "%DocumentsLocale%EZ.bat" >> "%DesktopLocale%RSOKR.vbs"
-echo oMyShortCut.Hotkey = "Ctrl+Alt+F4" >> "%DesktopLocale%RSOKR.vbs"
-echo oMyShortCut.Save >> "%DesktopLocale%RSOKR.vbs"
-echo, >> "%DesktopLocale%RSOKR.vbs"
-echo WshShell.run "attrib +h RSOKR.vbs",0 >> "%DesktopLocale%RSOKR.vbs"
-echo WshShell.run "attrib +h a_key.lnk",0 >> "%DesktopLocale%RSOKR.vbs"
-
-cd %DesktopLocale%
-cscript //nologo RSOKR.vbs
-
-echo 设置完成！按 Ctrl+Alt+F4 即可运行本脚本。
+echo 本脚本易被管理员删除，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
+echo 本脚本易被管理员删除，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
+echo 本脚本易被管理员删除，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
+msg * 本脚本易被管理员删除，建议用纸笔记下永久网址：ez.yxsw1802.com.cn
 goto Exiting
 
 :NoAccess
@@ -221,9 +240,8 @@ echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo 错误！没有权限运行。可尝试：
 echo 1. 使用右键〉以管理员身份运行。
 
-
 :Exiting
 echo,
-echo 按任意键退出本程序...
+echo 按任意键退出本脚本...
 pause >nul
 exit
--- README.md
+++ README.md
@@ -1,31 +1,20 @@
-# 红蜘蛛杀手(EZ-RSKiller)官方网站
-*没有之一*
+# 红蜘蛛杀手(EZ-RSKiller)
 
-[伽卡他卡杀手 绝赞发布中](https://gk.yxsw1802.com.cn)
-### 卧槽这啥我怕怕？我要回去。
-[哦](https://yxsw1802.com.cn)
-## 下载链接
-点击上方 .zip 按钮下载
-
-本脚本已经成功拷贝到学生可读文件夹。
-+ 计一三四室为：学生可读\EZ
-+ 计五六七室为：学生可读\install\Easy
+## 官方网站
+[ez.yxsw1802.com.cn](https://ez.yxsw1802.com.cn)
 
 ## 使用提示
-1.最好将本脚本解压并复制到D盘等非自动恢复磁盘任意处，防止文件被重置。
+1.最好将本脚本解压并复制到D盘等任意非自动恢复磁盘处，防止文件被删除。
 
-2.管理员可能会屑除本脚本，建议用纸笔记下本脚本永久网址：[ez.yxsw1802.com.cn](https://ez.yxsw1802.com.cn)。
+2.管理员可能会删除本脚本，建议用纸笔记下本脚本永久网址：[ez.yxsw1802.com.cn](https://ez.yxsw1802.com.cn)。
 
-3.如果已经被控制，请使用Ctrl+Alt+Delete键进入安全模式。点击"切换用户"，然后点击用户头像即可脱离控制。(该方法仅Windows7系统有效)
+3.如果已经被控制，请使用 `Ctrl+Alt+Delete` 键进入安全模式。点击“切换用户”，然后点击用户头像即可脱离控制。（该方法仅 Windows 7 系统有效）
 
-4.本网站无法访问？请使用21世纪的浏览器访问本网站：如[Google Chrome](https://www.google.cn/chrome/index.html)，[Firefox](http://www.firefox.com.cn/)，[Edge](https://www.microsoft.com/zh-cn/windows/microsoft-edge)，以及各种Chrome暴力换壳浏览器（如[360暴力换壳浏览器](https://browser.360.cn/)、[QQ暴力换壳浏览器](https://browser.qq.com)）等。生蛆Internet Explorer(IE)浏览器无法访问本网站。
+4.本网站无法访问？请使用 21 世纪的浏览器访问本网站。Internet Explorer(IE)浏览器无法访问本网站。
 
-5.谢绝无脑 anti 爆破本脚本。
 ## 开源许可协议
 This Source Code Form is subject to the terms of the Mozilla Public
-
 License, v. 2.0. If a copy of the MPL was not distributed with this
-
 file, You can obtain one at [https://mozilla.org/MPL/2.0/](https://www.mozilla.org/MPL/2.0/).
 
 本源代码受 [Mozilla Public 条款 v. 2.0](https://www.mozilla.org/MPL/2.0/) 协议的约束。
